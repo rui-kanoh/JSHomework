16600e510fbc02b5f8627f42c3589d41d67bc6b0
rui-kanoh <rui.kanoh@gmail.com>
2019-09-15 06:47:22 +0900

    quickSort

--- task5/js/index.js
+++ task5/js/index.js
@@ -21,7 +21,42 @@ function bubble(array) {
   return sortedArray
 }
 
+const sorted = []
+function quick(a) {
+  const ary = Array.from(a)
+  if (ary.length === 0) {
+  } else if (ary.length === 1) {
+    sorted.push(ary[0])
+  } else if (ary.length === 2) {
+    sorted.push(ary[0] > ary[1] ? ary[0] : ary[1])
+    sorted.push(ary[0] > ary[1] ? ary[1] : ary[0])
+  } else {
+    let pivot = 0
+    let arrayU = []
+    let arrayL = []
+    for (let i = 0; i < ary.length; ++i) {
+      pivot = ary[i]
+      arrayU = ary.filter(item => item > pivot)
+      arrayL = ary.filter(item => item <= pivot)
+      if (arrayU.length !== 0 && arrayL.length !== 0) {
+        break
+      }
+    }
+
+    quick(Array.from(arrayU))
+    quick(Array.from(arrayL))
+  }
+}
+
 const sortedArray = bubble(numbers)
 sortedArray.forEach(item => {
+  // console.log(item)
+})
+
+const numbers2 = [20, 31, 42, 13, 5, 38]
+quick(Array.from(numbers2))
+
+console.log('sorted')
+sorted.forEach(item => {
   console.log(item)
 })
